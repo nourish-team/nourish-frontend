65e0f75d3d03902f2f6171c0aaf481ae557d7909
oddpharmacy <107352832+oddpharmacy@users.noreply.github.com>
2023-06-17 21:47:30 +0900

    feat: implement delete products --- style: revamp user routine page screen

--- src/screens/UserRoutinePageScreen.tsx
+++ src/screens/UserRoutinePageScreen.tsx
@@ -1,5 +1,13 @@
 import React, { useState, useEffect, useCallback } from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ImageBackground,
+  ScrollView,
+  Image,
+} from "react-native";
 import { RouteProp } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from "../navigation/types";
@@ -25,7 +33,9 @@ type Props = {
 
 const UserRoutinePageScreen: React.FC<Props> = ({ route, navigation }) => {
   const { routineId, routineName, routineProduct } = route.params;
-  const [products, setProducts] = useState<string[]>([]);
+  const [products, setProducts] = useState<
+    { productName: string; productBrand: string; productId: number }[]
+  >([]);
   const [loading, setLoading] = useState(true);
 
   const handleBackPress = () => {
@@ -48,6 +58,37 @@ const UserRoutinePageScreen: React.FC<Props> = ({ route, navigation }) => {
     });
   };
 
+  const handleDeleteProduct = async (itemId: number) => {
+    const productsList = products.map((product) => product.productId);
+    const newRoutineProducts = productsList.filter(
+      (product) => product !== itemId
+    );
+    console.log("newroutine stuff ", newRoutineProducts);
+    const routineData = {
+      routine_id: routineId,
+      routine_product: newRoutineProducts,
+      public: true,
+    };
+    try {
+      const response = await fetch(`http://10.0.2.2:8080/routine/update`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(routineData),
+      });
+
+      if (response.ok) {
+        alert("Product deleted.");
+        setProducts(products.filter((product) => product.productId !== itemId));
+      } else {
+        alert("Oops, something went wrong. Please try again.");
+      }
+    } catch (error) {
+      alert("Oops, something went wrong. Please try again.");
+    }
+  };
+
   const fetchAndDisplayProducts = useCallback(async () => {
     setLoading(true);
     setProducts([]);
@@ -56,7 +97,11 @@ const UserRoutinePageScreen: React.FC<Props> = ({ route, navigation }) => {
       const fetchPromises = routineProduct.map(async (pid) => {
         const response = await fetch(`http://10.0.2.2:8080/product/id/${pid}`);
         const data = await response.json();
-        const newProduct = data.product_name;
+        const newProduct = {
+          productName: data.product_name,
+          productBrand: data.brand,
+          productId: data.id,
+        };
         return newProduct;
       });
 
@@ -78,65 +123,118 @@ const UserRoutinePageScreen: React.FC<Props> = ({ route, navigation }) => {
     <View style={styles.container}>
       <View style={styles.titleContainer}>
         <TouchableOpacity onPress={handleBackPress}>
-          <AntDesign name="doubleleft" size={30} color="rgba(1,90,131,255)" />
+          <AntDesign name="doubleleft" size={20} color="rgba(1,90,131,255)" />
         </TouchableOpacity>
         <Text style={[styles.titleText, styles.buttonMargin]}>
           {routineName}
         </Text>
       </View>
-      <View style={styles.buttonContainer}>
-        <View style={styles.buttonRow}>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={handleAddJournalPress}
+      <ScrollView>
+        <View style={styles.contentContainer}>
+          <View style={styles.descriptionContainer}>
+            <View style={styles.descriptionContainerTop}>
+              <Text style={styles.descriptionTitleText}>description</Text>
+              <Text
+                style={styles.descriptionTitleText}
+                onPress={handleBackPress}
+              >
+                ⊹⊹⊹
+              </Text>
+            </View>
+            <View style={styles.descriptionContainerBottom}></View>
+          </View>
+          <Text style={styles.separator}>⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹</Text>
+          <View style={styles.textBubbleContainer}>
+            <View style={styles.textBubble}>
+              <Text style={styles.textBubbleText}>
+                how was your {"\n"}skin today?
+              </Text>
+            </View>
+            <View style={styles.leftArrow} />
+            <Image
+              source={require("../../assets/images/nourish_logo.png")}
+              style={styles.tinyLogo}
+            />
+          </View>
+          <ImageBackground
+            source={require("../../assets/images/journal-withtext.png")}
+            style={styles.imageContainer}
+            resizeMode="contain"
           >
-            <Text style={styles.buttonText}>Add Journal</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={[styles.button, styles.buttonMargin]}
-            onPress={handleJournalHistoryPress}
-          >
-            <Text style={styles.buttonText}>Journal History</Text>
-          </TouchableOpacity>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={handleAddJournalPress}
+              >
+                <Text style={styles.buttonText}>
+                  Add{"\n"}Journal{"\n"}Entry
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={handleJournalHistoryPress}
+              >
+                <Text style={styles.buttonText}>Journal{"\n"}History</Text>
+              </TouchableOpacity>
+            </View>
+          </ImageBackground>
+          <Text style={styles.separator}>⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹⊹</Text>
+          <View style={styles.productsContainer}>
+            <View style={styles.productsContainerHeader}>
+              <Text style={styles.productsContainerTitle}>
+                {products.length}
+                {products.length > 1 ? <Text> steps</Text> : <Text> step</Text>}
+              </Text>
+              <TouchableOpacity onPress={handleAddProduct}>
+                <Text style={styles.infoText}>+ Add</Text>
+              </TouchableOpacity>
+            </View>
+            {products.map((product, index) => (
+              <View key={index} style={styles.card}>
+                <View style={styles.brandContainer}>
+                  <Text style={styles.brandName}>
+                    {product.productBrand.toUpperCase()}
+                  </Text>
+                  <Text
+                    style={styles.brandName}
+                    onPress={() => handleDeleteProduct(product.productId)}
+                  >
+                    ✖︎
+                  </Text>
+                </View>
+                <Text style={styles.productName}>{product.productName}</Text>
+              </View>
+            ))}
+          </View>
         </View>
-      </View>
-      <TouchableOpacity onPress={handleAddProduct}>
-        <Text style={styles.infoText}>Add Product</Text>
-      </TouchableOpacity>
-      {products.map((product, index) => (
-        <Text key={index} style={styles.card}>
-          {product}
-        </Text>
-      ))}
+      </ScrollView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 80,
+    paddingTop: 50,
     flex: 1,
-    backgroundColor: "#FFFDD0",
-    paddingLeft: 20,
-    paddingRight: 20,
+    backgroundColor: "#EEE3CB",
   },
   titleContainer: {
     flexDirection: "row",
     alignItems: "flex-start",
     marginBottom: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
   },
   tinyLogo: {
-    width: 40,
-    height: 40,
-    marginTop: -7,
+    width: 70,
+    height: 70,
   },
   titleText: {
-    textAlign: "left",
-    fontSize: 40,
+    fontSize: 30,
     fontFamily: "PlayfairDisplay-Bold",
     color: "rgba(1,90,131,255)",
-    marginBottom: 20,
-    marginTop: -20,
+    marginBottom: 10,
+    marginTop: -15,
   },
   line: {
     width: "100%",
@@ -148,42 +246,149 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 18,
     fontFamily: "Lato-Bold",
-    color: "rgba(1,90,131,255)",
+    color: "white",
     lineHeight: 25,
-    marginBottom: 30,
-    marginTop: 30,
+    marginRight: 5,
+    borderWidth: 2,
+    borderRadius: 10,
+    borderColor: "rgba(1,90,131,255)",
+    backgroundColor: "rgba(1,90,131,255)",
+    paddingTop: 8,
+    paddingBottom: 8,
+    paddingLeft: 15,
+    paddingRight: 15,
+  },
+  contentContainer: {
+    backgroundColor: "white",
+    width: "100%",
+    height: "100%",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+  },
+  descriptionContainer: {
+    margin: 40,
+  },
+  descriptionContainerTop: {
+    height: 40,
+    borderColor: "rgba(1,90,131,255)",
+    borderTopWidth: 3,
+    borderLeftWidth: 3,
+    borderRightWidth: 3,
+    backgroundColor: "#B7C4CF",
+    justifyContent: "space-between",
+    flexDirection: "row",
+  },
+  descriptionContainerBottom: {
+    backgroundColor: "white",
+    padding: 40,
+    borderColor: "rgba(1,90,131,255)",
+    borderWidth: 3,
+  },
+  descriptionTitleText: {
+    fontFamily: "Lato-Bold",
+    padding: 10,
+    color: "black",
+    fontSize: 15,
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    alignSelf: "center",
+    flex: 1,
+    justifyContent: "center",
   },
   buttonContainer: {
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  buttonRow: {
     flexDirection: "row",
     justifyContent: "space-around",
-    marginBottom: 20,
+    marginTop: -40,
   },
   button: {
-    width: 150,
-    height: 50,
-    backgroundColor: "rgba(1,90,131,255)",
+    flex: 1,
+    width: "60%",
+    height: "100%",
+    backgroundColor: "transparent",
     justifyContent: "center",
     alignItems: "center",
   },
   buttonText: {
-    color: "white",
+    color: "transparent",
     fontSize: 16,
-    fontFamily: "Lato-Bold",
+    fontFamily: "PlayfairDisplay-Bold",
+    textAlign: "center",
   },
   buttonMargin: {
     marginLeft: 25,
   },
   card: {
-    backgroundColor: "#FFF",
+    backgroundColor: "white",
     padding: 10,
     marginBottom: 10,
-    borderRadius: 5,
-    borderWidth: 1,
+    borderRadius: 15,
+    borderWidth: 2,
     borderColor: "rgba(1, 90, 131, 255)",
+  },
+  separator: {
+    color: "rgba(1,90,131,255)",
+    fontSize: 15,
+    textAlign: "center",
+  },
+  productsContainer: {
+    margin: 30,
+  },
+  productsContainerHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  productsContainerTitle: {
+    fontSize: 25,
+    fontFamily: "PlayfairDisplay-Bold",
+    backgroundColor: "#EEE3CB",
+    width: 100,
+    textAlign: "center",
+    borderRadius: 30,
+    padding: 5,
+    marginBottom: 20,
+  },
+  brandContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  brandName: {
+    color: "gray",
+    fontFamily: "PlayfairDisplay-Bold",
+  },
+  productName: {
+    color: "rgba(1, 90, 131, 255)",
+    fontFamily: "Lato-Bold",
+  },
+  textBubbleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    margin: 30,
+    marginBottom: -10,
+    alignSelf: "flex-end",
+  },
+  leftArrow: {
+    width: 0,
+    height: 0,
+    backgroundColor: "transparent",
+    borderStyle: "solid",
+    borderRightWidth: 10,
+    borderTopWidth: 10,
+    borderRightColor: "transparent",
+    borderTopColor: "#D0D0D0",
+    transform: [{ rotate: "270deg" }],
+  },
+  textBubble: {
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    backgroundColor: "#D0D0D0",
+    borderRadius: 10,
+    maxWidth: "80%",
+  },
+  textBubbleText: {
+    fontSize: 16,
   },
 });
 
