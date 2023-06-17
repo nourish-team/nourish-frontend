2df35e43379a73fab56aff52e9750622b8d69bca
oddpharmacy <107352832+oddpharmacy@users.noreply.github.com>
2023-06-17 14:16:50 +0900

    feat: import fonts

--- App.tsx
+++ App.tsx
@@ -42,12 +42,14 @@ export default function App() {
   // Helpers
   const fetchFonts = () => {
     return Font.loadAsync({
+      "PlayfairDisplay-Black": require("./assets/fonts/PlayfairDisplay-Black.ttf"),
       "PlayfairDisplay-Bold": require("./assets/fonts/PlayfairDisplay-Bold.ttf"),
       "PlayfairDisplay-BoldItalic": require("./assets/fonts/PlayfairDisplay-BoldItalic.ttf"),
       "PlayfairDisplay-Medium": require("./assets/fonts/PlayfairDisplay-Medium.ttf"),
       "PlayfairDisplay-Regular": require("./assets/fonts/PlayfairDisplay-Regular.ttf"),
       "Lato-Bold": require("./assets/fonts/Lato-Bold.ttf"),
       "Lato-Regular": require("./assets/fonts/Lato-Regular.ttf"),
+      "Lato-BoldItalic": require("./assets/fonts/Lato-BoldItalic.ttf"),
     });
   };
 
