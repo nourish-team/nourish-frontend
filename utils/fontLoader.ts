8c798ee8d6407a66def81741063e0d49351e9c7f
oddpharmacy <107352832+oddpharmacy@users.noreply.github.com>
2023-08-16 06:58:36 +0800

    refactor: move font loader to separate file

---
+++ utils/fontLoader.ts
@@ -0,0 +1,14 @@
+import * as Font from "expo-font";
+
+export const loadFonts = async () => {
+  return Font.loadAsync({
+    "PlayfairDisplay-Black": require("./assets/fonts/PlayfairDisplay-Black.ttf"),
+    "PlayfairDisplay-Bold": require("./assets/fonts/PlayfairDisplay-Bold.ttf"),
+    "PlayfairDisplay-BoldItalic": require("./assets/fonts/PlayfairDisplay-BoldItalic.ttf"),
+    "PlayfairDisplay-Medium": require("./assets/fonts/PlayfairDisplay-Medium.ttf"),
+    "PlayfairDisplay-Regular": require("./assets/fonts/PlayfairDisplay-Regular.ttf"),
+    "Lato-Bold": require("./assets/fonts/Lato-Bold.ttf"),
+    "Lato-Regular": require("./assets/fonts/Lato-Regular.ttf"),
+    "Lato-BoldItalic": require("./assets/fonts/Lato-BoldItalic.ttf"),
+  });
+};
