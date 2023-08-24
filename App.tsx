44b173fe211092e44bb50a49ede21fcf6a64146e
oddpharmacy <107352832+oddpharmacy@users.noreply.github.com>
2023-08-24 12:45:13 +0800

    refactor: app.tsx

--- App.tsx
+++ App.tsx
@@ -1,73 +1,40 @@
 // Library and package imports
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { createStackNavigator } from "@react-navigation/stack";
 import React, { useEffect, useState } from "react";
-import * as Font from "expo-font";
 
 // Authentication
-import { useAuthentication } from "./utils/hooks/useAuthentication";
 import "./config/firebase";
 
-// Components
-import RootNavigation from "./src/navigation";
-import LoadingScreen from "./src/screens/LoadingScreen";
+// Nav
+import RootNav from "./src/navigation/root";
 
 // Context
-import { AppContext } from "./src/contexts/AppContext";
-import UserContext from "./src/contexts/UserContext";
+import { UserProvider } from "./src/contexts/UserContext";
+
+// Font
+import { loadFonts } from "./utils/fontLoader";
 
 export default function App() {
-  // States
-  const [isLoading, setIsLoading] = useState(true);
-  const [userId, setUserId] = useState<string | null>(null);
-  const [userName, setUserName] = useState<string | null>(null);
-
-  // Use Effects
-  // Loading Delay for the looks
   useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
-  }, []);
-
-  useEffect(() => {
-    const loadFonts = async () => {
-      await fetchFonts();
+    const fontSetup = async () => {
+      try {
+        await loadFonts();
+      } catch (error) {
+        console.error("Error loading fonts: ", error);
+      }
     };
-    loadFonts();
+    fontSetup();
   }, []);
-
-  // Helpers
-  const fetchFonts = () => {
-    return Font.loadAsync({
-      "PlayfairDisplay-Black": require("./assets/fonts/PlayfairDisplay-Black.ttf"),
-      "PlayfairDisplay-Bold": require("./assets/fonts/PlayfairDisplay-Bold.ttf"),
-      "PlayfairDisplay-BoldItalic": require("./assets/fonts/PlayfairDisplay-BoldItalic.ttf"),
-      "PlayfairDisplay-Medium": require("./assets/fonts/PlayfairDisplay-Medium.ttf"),
-      "PlayfairDisplay-Regular": require("./assets/fonts/PlayfairDisplay-Regular.ttf"),
-      "Lato-Bold": require("./assets/fonts/Lato-Bold.ttf"),
-      "Lato-Regular": require("./assets/fonts/Lato-Regular.ttf"),
-      "Lato-BoldItalic": require("./assets/fonts/Lato-BoldItalic.ttf"),
-    });
-  };
-
-  // Loading screen
-  if (isLoading) {
-    return <LoadingScreen />;
-  }
 
   return (
     <>
       <StatusBar style="light" backgroundColor="#B7C4CF" />
-      <UserContext.Provider
-        value={{ userId, setUserId, userName, setUserName }}
-      >
+      <UserProvider>
         <NavigationContainer>
-          <RootNavigation />
+          <RootNav />
         </NavigationContainer>
-      </UserContext.Provider>
+      </UserProvider>
     </>
   );
 }
