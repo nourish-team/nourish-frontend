21b46eff04f9cf45af644247b891e2c17d05b9d5
oddpharmacy <107352832+oddpharmacy@users.noreply.github.com>
2023-08-24 12:46:59 +0800

    feat: global user context

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,17 +1,44 @@
-import React, {createContext} from 'react';
+import React, { createContext, useState, ReactNode, useContext } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 interface UserContextType {
-  userId: string | null;
-  setUserId: (userId: string | null) => void;
-  userName: string | null;
-  setUserName: (userName: string | null) => void;
+  username: string | null;
+  loadUsername: () => void;
+  saveUsername: (newUsername: string) => void;
 }
 
-const UserContext = createContext<UserContextType>({
-  userId: null,
-  setUserId: () => {},
-  userName: null,
-  setUserName: () => {},
-});
+const UserContext = createContext<UserContextType | undefined>(undefined);
 
-export default UserContext;
+interface UserProviderProps {
+  children: ReactNode;
+}
+
+export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
+  const [username, setUsername] = useState<string | null>(null);
+
+  const loadUsername = async () => {
+    const storedUsername = await AsyncStorage.getItem("username");
+    if (storedUsername) {
+      setUsername(storedUsername);
+    }
+  };
+
+  const saveUsername = async (newUsername: string) => {
+    await AsyncStorage.setItem("username", newUsername);
+    setUsername(newUsername);
+  };
+
+  return (
+    <UserContext.Provider value={{ username, loadUsername, saveUsername }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (context) {
+    return context;
+  }
+  throw new Error("useUser must be used within a useProvider!");
+};
